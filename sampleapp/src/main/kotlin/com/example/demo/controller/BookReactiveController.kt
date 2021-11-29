18d247140b0647f2922fb22cf7dbcd178057304f
shinyay <shinya.com@gmail.com>
2021-11-29 13:47:03 +0900

    Add Reactive Controller

--- sampleapp/src/main/kotlin/com/example/demo/controller/BookReactiveController.kt
+++ sampleapp/src/main/kotlin/com/example/demo/controller/BookReactiveController.kt
@@ -1,4 +1,26 @@
 package com.example.demo.controller
 
-class BookReactiveController {
+import com.example.demo.entity.Book
+import com.example.demo.repository.BookRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+@Configuration
+class BookReactiveController(val repository: BookRepository) {
+
+    @Bean
+    fun router(): RouterFunction<ServerResponse> {
+        return route()
+            .GET("/books", this::findAllHandler)
+            .build()
+    }
+
+    fun findAllHandler(request: ServerRequest): Mono<ServerResponse> {
+        return ServerResponse.ok().body(repository.findAll(), Book::class.java)
+    }
 }
