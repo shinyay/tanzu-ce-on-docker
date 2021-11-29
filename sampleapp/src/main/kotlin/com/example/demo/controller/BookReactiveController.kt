2883e85d2f773fc68af8bd312a130af521bb7a86
shinyay <shinya.com@gmail.com>
2021-11-29 16:19:46 +0900

    Add adding function for POST

--- sampleapp/src/main/kotlin/com/example/demo/controller/BookReactiveController.kt
+++ sampleapp/src/main/kotlin/com/example/demo/controller/BookReactiveController.kt
@@ -4,6 +4,7 @@ import com.example.demo.entity.Book
 import com.example.demo.repository.BookRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerRequest
@@ -22,5 +23,14 @@ class BookReactiveController(val repository: BookRepository) {
 
     fun findAllHandler(request: ServerRequest): Mono<ServerResponse> {
         return ServerResponse.ok().body(repository.findAll(), Book::class.java)
+    }
+
+    fun createHandler(request: ServerRequest): Mono<ServerResponse> {
+        val book = request.bodyToMono(Book::class.java)
+        return book.flatMap { it ->
+            ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(repository.save(it), Book::class.java) 
+        }
     }
 }
